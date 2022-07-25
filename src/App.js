7e037b8bb3d8f11e32e7e0ed303cbe3c5b61290d
LucasSlv <lucassilvax005x@gmail.com>
2022-07-24 22:12:36 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import TodoList from './components/List';
+
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
